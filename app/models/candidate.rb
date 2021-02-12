789ef523dfeedbd6c838c9b6d994849046df0ca8
Charles Martinez <charles@ikbenfrits.nl>
2021-02-12 16:12:13 +0800

    Updated model validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,5 @@
 class Candidate < ApplicationRecord
 
-  validates :first_name, :last_name, :email, :phone, presence: true, uniqueness: true
+  validates :first_name, :last_name, :email, :phone, presence: true
+  validates :email, uniqueness: true
 end
--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
   describe 'validations' do
-    it { is_expected.to validate_uniqueness_of(:first_name) }
-    it { is_expected.to validate_uniqueness_of(:last_name) }
     it { is_expected.to validate_uniqueness_of(:email) }
-    it { is_expected.to validate_uniqueness_of(:phone) }
 
 
     it { is_expected.to validate_presence_of(:first_name) }
